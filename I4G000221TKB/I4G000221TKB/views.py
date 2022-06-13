30251d3df2a3a2c891e1269ec8aad26ecce8d604
mirera <mirera.moses@gmail.com>
2022-06-14 00:34:44 +0300

    Created 14G000221TKB project

---
+++ I4G000221TKB/I4G000221TKB/views.py
@@ -0,0 +1,10 @@
+from ast import Return
+from django.http import HttpResponse 
+
+def homepage(request):
+    return HttpResponse('homepage')
+
+
+def about(request):
+    return HttpResponse('about')
+
