ad040519412c17d9967b167fcf405bae2910334a
Mirera Nyaundi <mirera.moses@gmail.com>
2022-06-21 15:05:19 +0300

    Update models.py

--- I4G000221TKB/blog/models.py
+++ I4G000221TKB/blog/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
-    #author = models.ForeignKey(User)
+    #author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add=True)
     published_date = models.DateTimeField(auto_now_add=True)
 
